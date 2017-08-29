2afbf9dcc4748808289eb1a37c2b35ec1a62e21f
Tommy Savaria <tommy.savaria@protonmail.ch>
2017-08-29 14:01:22 -0400

    Enlevé toute mention de la promocarte et de son prix

--- app/models/User.js
+++ app/models/User.js
@@ -56,8 +56,7 @@ class User extends Model {
       }
     },
     totalPoints: { type: Number },
-    promocard: { type: Object, shape: {
-        price: { type: Number },
+    authorization: { type: Object, shape: {
         date: { type: Date },
       },
     },
