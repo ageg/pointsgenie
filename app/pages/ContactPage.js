3e208897fe1cbe020ef9cf9e7da2b187368fc0d0
rdrouin <shauln_1994@hotmail.com>
2017-10-25 19:50:55 -0700

    Update contacts for promo60

--- app/pages/ContactPage.js
+++ app/pages/ContactPage.js
@@ -10,54 +10,51 @@ class ContactPage extends Component {
 
         <h3>Comité exécutif</h3>
         <p>
-          <b>Président</b> - <a href="mailto:gabriel.courtemanche@usherbrooke.ca">Gabriel Courtemanche</a> <br/>
-          <b>Vice-Président, Directeur Points Genie</b> - <a href="mailto:Christian-roch.faucher@usherbrooke.ca">Rocko Faucher</a> <br/>
-          <b>Secrétaire</b> - <a href="mailto:vincent.marcotte2@usherbrooke.ca">Vincent Marcotte</a> <br/>
-          <b>Trésorier</b> - <a href="mailto:guy-samuel.fournier@usherbrooke.ca">Guy-Samuel Fournier</a> <br/>
-          <b>Webmestre</b> - <a href="mailto:laurent.huot2@usherbrooke.ca">Laurent Huot</a> <br/>
-          <b>Webmestre</b> - <a href="mailto:guillaume.paquet@usherbrooke.ca">Guillaume Paquet</a> <br/>
-          <b>Directeurs 5@8</b> - <a href="mailto:5a8@ageg.ca">Alexandre Carbonneau et Gabrielle Fauteux Cormier	</a> <br/>
-          <b>Directeur Affaires Sociales</b> - <a href="mailto:david.v.vachon@usherbrooke.ca">David Vachon</a> <br/>
-          <b>Directrice Bal et Voyage</b> - <a href="mailto:laurie-eve.beaulieu@usherbrooke.ca">Laurie-Eve Beaulieu</a> <br/>
-          <b>Directrice Souvenirs</b> - <a href="mailto:catherine.thiffault@usherbrooke.ca">Catherine Thiffault</a> <br/>
-          <b>Directrice Jonc et Casino</b> - <a href="mailto:eliane.fortin@usherbrooke.ca">Éliane Fortin</a>
-        </p> 
-    
+          <b>Présidente</b> - <a href="mailto:Catherine.nadeau4@usherbrooke.ca">Catherine Nadeau</a> <br/>
+          <b>Vice-Président, Directeur Points Genie</b> - <a href="mailto:Beatrice.laroche@usherbrooke.ca ">Béatrice Laroche</a> <br/>
+          <b>Secrétaire</b> - <a href="mailto:Antoine.perras.cote@usherbrooke.ca">Antoine Perras Côté</a> <br/>
+          <b>Trésorier</b> - <a href="mailto:Alexandre.hetu@usherbrooke.ca">Alexandre Hétu</a> <br/>
+          <b>Webmestre</b> - <a href="mailto:Raphael.drouin@usherbrooke.ca">Raphaël Drouin</a> <br/>
+          <b>Directeurs 5@8</b> - <a href="mailto:5a8@ageg.ca">Pierre-Olivier Reneault et Phillipe Rivard</a> <br/>
+          <b>Directeur Affaires Sociales</b> - <a href="mailto:Alexandre.girard4@usherbrooke.ca">Alexandre Girard</a> <br/>
+          <b>Directrice Bal et Voyage</b> - <a href="mailto:Laurence.Degre@usherbrooke.ca">Laurence Degré</a> <br/>
+          <b>Directrice Souvenirs</b> - <a href="mailto:vanessa.wanie@usherbrooke.ca">Vanessa Wanie</a> <br/>
+          <b>Directeur Jonc et Casino</b> - <a href="mailto:sebastien.bosse@usherbrooke.ca">Sébastien Bossé</a>
+        </p>
+
         <h3>Comité 5@8</h3>
         <p>
-          <b>Directeurs 5@8</b> - <a href="mailto:5a8@ageg.ca">Alexandre Carbonneau et Gabrielle Fauteux Cormier</a> <br/>
-          <b>Sous-Directeur Animation </b> - <a href="mailto:Francis.Turcotte3@usherbrooke.ca">Francis Turcotte</a> <br/>
-          <b>Sous-Directeur Bière </b> - <a href="mailto:nicolas.brissette@usherbrooke.ca">Nicolas Brissette</a> <br/>
-          <b>Sous-Directeur Bière</b> - <a href="mailto:Mathieu.chabot@usherbrooke.ca">Mathieu Chabot</a> <br/>
-          <b>Sous-Directrice Bouffe</b> - <a href="mailto:antoine.macia@usherbrooke.ca">Antoine Macia</a> <br/>
-          <b>Sous-Directeur Caisse</b> - <a href="mailto:Jo-eve.legault.champagne@usherbrooke.ca">Jo-Eve Champagne</a> <br/>
-          <b>Sous-Directrice Fort</b> - <a href="mailto:camille.trudeau@usherbrooke.ca">Camille Trudeau</a> <br/>      
-          <b>Sous-Directeur Fort</b> - <a href="mailto:Charles-etienne.godbout@usherbrooke.ca">Charles-Étienne Godbout</a> <br/>
-          <b>Sous-Directeur Sécurité</b> - <a href="mailto:jordan.lussier@usherbrooke.ca">Jordan Lussier</a> <br/>
-          <b>Sous-Directrice Sécurité</b> - <a href="mailto:Laurence.champagne@usherbrooke.ca">Laurence Beauvais</a> <br/>
-          <b>Sous-Directeur Radio</b> - <a href="mailto:guillaume.cote3@usherbrooke.ca">Guillaume Côté</a> <br/>
-          <b>Sous-Directeur Radio</b> - <a href="mailto:david.kabanza@usherbrooke.ca">David Kabanza</a>
+          <b>Directeurs 5@8</b> - <a href="mailto:5a8@ageg.ca">Pierre-Olivier Reneault et Phillipe Rivard</a> <br/>
+          <b>Sous-Directeur Animation </b> - <a href="mailto:Fancois.Pontbriand@usherbrooke.ca">François Pontbriand</a> <br/>
+          <b>Sous-Directeur Bière </b> - <a href="mailto:william.roy3@usherbrooke.ca">William Roy</a> <br/>
+          <b>Sous-Directeur Bière</b> - <a href="mailto:Alexandre.savard3@usherbrooke.ca">Alexandre Savard</a> <br/>
+          <b>Sous-Directeur Bouffe</b> - <a href="mailto:yves.blain-king@usherbrooke.ca">Yves Blain-King</a> <br/>
+          <b>Sous-Directeur Caisse</b> - <a href="mailto:francis.lacasse3@usherbrooke.ca">Francis Lacasse</a> <br/>
+          <b>Sous-Directrice Fort</b> - <a href="mailto:Laurence.demers-Lanouette@usherbrooke.ca">Laurence Demers-Lanouette</a> <br/>
+          <b>Sous-Directeur Fort</b> - <a href="mailto:Raphael.guerette@usherbrooke.ca">Raphaël Guerette</a> <br/>
+          <b>Sous-Directeur Sécurité</b> - <a href="mailto:Remy.rahem@usherbrooke.ca">Remy Rahem</a> <br/>
+          <b>Sous-Directeur Radio</b> - <a href="mailto:Jeremi.bourdeau@usherbrooke.ca">Jeremi Bourdeau</a> <br/>
+          <b>Sous-Directrice Photographie</b> - <a href="mailto:Sarah.desmarais@usherbrooke.ca">Sarah Desmarais</a>
         </p>
 
         <h3>Comité Bal et Voyage</h3>
         <p>
-          <b>Directrice</b> - <a href="mailto:laurie-eve.beaulieu@usherbrooke.ca">Laurie-Eve Beaulieu</a> <br/>
-          <b>Membre</b> - <a href="mailto:kelly.dufour@usherbrooke.ca">Kelly Dufour</a> <br/>
-          <b>Membre</b> - <a href="mailto:genevieve.lamont@usherbrooke.ca">Geneviève Lamont</a> <br/>
-          <b>Membre</b> - <a href="mailto:mathieu.rioux@usherbrooke.ca">Mathieu Rioux</a> <br/>
-          <b>Membre</b> - <a href="mailto:valerie.dube6@usherbrooke.ca">Valérie Dubé</a> <br/>
-          <b>Membre</b> - <a href="mailto:vincent.cote5@usherbrooke.ca">Vincent Côté</a>
+          <b>Directrice</b> - <a href="mailto:Laurence.Degre@usherbrooke.ca">Laurence Degré</a> <br/>
+          <b>Membre</b> - <a href="mailto:sarah.desmarais@usherbrooke.ca">Sarah Desmarais</a> <br/>
+          <b>Membre</b> - <a href="mailto:Louis-Philippe.lebel@usherbrooke.ca">Louis-Philippe Lebel</a>
         </p>
 
         <h3>Comité Souvenirs</h3>
         <p>
-          <b>Directrice</b> - <a href="mailto:catherine.thiffault@usherbrooke.ca">Catherine Thiffault</a> <br/>
-          <b>Membre</b> - <a href="mailto:jasmine.corriveau@usherbrooke.ca">Jasmine Corriveau</a><br/>
-          <b>Membre</b> - <a href="mailto:emie.perreault@usherbrooke.ca">Emie Perreault</a><br/>
-          <b>Membre</b> - <a href="mailto:marie-eve.mongeon@usherbrooke.ca">Marie-Ève Mongeon</a><br/>
-          <b>Membre</b> - <a href="mailto:rosalie.lessard@usherbrooke.ca">Rosalie Lessard</a>
+          <b>Directrice</b> - <a href="mailto:vanessa.wanie@usherbooke.ca">Vanessa Wanie</a> <br/>
+          <b>Membre</b> - <a href="mailto:jessica.samson2@usherbrooke.ca">Jessica Samson</a>
         </p>
-      </div>  
+
+        <h3>Comité Jonc/Casino</h3>
+        <p>
+          <b>Directeur</b> - <a href="mailto:sebastien.bosse@usherbooke.ca">Sébastien Bossé</a>
+        </p>
+      </div>
     );
   }
 };
--- app/pages/FAQPage.js
+++ app/pages/FAQPage.js
@@ -10,29 +10,29 @@ class FAQPage extends Component {
       <div className="faq">
         <h3>Foire aux questions</h3>
         <h5>S.V.P veuillez consulter la charte pour avoir plus d'informations:</h5>
-        <Button bsStyle="primary" href="/charte-e17.pdf">Charte</Button>
+        <Button bsStyle="primary" href="/charte-a17.pdf">Charte</Button>
         <hr/>
 
         <h4>À quoi servent les Points Génie?</h4>
         <blockquote>Les Points Génie permettront, en bonne partie, de payer les différents frais reliés à la graduation pour la promotion finissante (Photo des finissants, album, voyage de la finissante, Jonc, etc.)</blockquote>
 
         <h4>Pourquoi avons-nous une promo carte?</h4>
-        <blockquote>La promocarte permet aux étudiantes de la promotion finissante de postuler à des événements tels que les 5@8. Celle-ci est traditionnellement attibuée par le Directeur Point Génie à tous les étudiants figurant sur la liste de la promotion de la faculté. Cette carte est strictement virtuelle et n'est pas transférable.</blockquote>
+        <blockquote>La promocarte permet aux étudiantes de la promotion finissante de postuler à des événements tels que les 5@8. Celle-ci est traditionnellement attibuée par le Directrice Point Génie à tous les étudiants figurant sur la liste de la promotion de la faculté. Cette carte est strictement virtuelle et n'est pas transférable.</blockquote>
 
         <h4>Combien vaut un Point Génie ?</h4>
-        <blockquote>La valeur d’un Point Génie sera seulement connue au mois de novembre 2017, car la valeur du Point Génie est calculée en fonction du montant total accumulé par la promotion finissante ainsi que du nombre de points distribués.</blockquote>
+        <blockquote>La valeur d’un Point Génie sera seulement connue au mois de novembre 2018, car la valeur du Point Génie est calculée en fonction du montant total accumulé par la promotion finissante ainsi que du nombre de points distribués. Le calcul pour évaluer la valeur du point génie est présenté dans le règlement 6 de la charte de la 60e promotion.</blockquote>
 
         <h4>Comment les personnes travaillant pour un évènement sont-elles choisies ?</h4>
-        <blockquote>Sur toutes les personnes ayant appliqué, ce sont celles ayant le moins de Points Génie qui seront priorisées (à une différence de 3 points). Le choix se fera aussi en fonction des tâches préférées. 
+        <blockquote>Sur toutes les personnes ayant appliqué, ce sont celles ayant le moins de Points Génie qui seront priorisées (à une différence de 3 points). Le choix se fera aussi en fonction des tâches préférées.
         Par contre, notez que les tâches préférées sont assignées en fonction des disponibilités des postes et non en fonction des Points Génie et que celles-ci ne sont que de nature informative.
-        Pour plus d'information sur l'affichage des postes, vous référer à la charte de la 59e promotion ou contacter votre Directeur Points Génie.</blockquote>
- 
+        Pour plus d'information sur l'affichage des postes, vous référer à la charte de la 60e promotion ou contacter votre Directrice Points Génie.</blockquote>
+
         <h4>À partir de quand est-il possible de postuler pour un évènement?</h4>
         <blockquote>Pour les 5@8, la phase de postulation débutera le dimanche 18h jusqu’à mardi 18h la semaine courante de l’évènement.
         Pour les autres évènements, le début de la phase de postulation sera annoncée par courriel USherbrooke.</blockquote>
-      
+
         <h4>Qui dois-je contacter en cas de problème ou si j'ai des questions? </h4>
-        <blockquote>Pour ce qui est de la distribution des Points Génie, contacter Philippe St-Louis ou Marie-Pier Gaudet, Directeurs Points Génie. Pour ce qui est en lien avec le fonctionnement du site web, contacter Laurent Huot ou Guillaume Paquet, Webmestres. </blockquote>
+        <blockquote>Pour ce qui est de la distribution des Points Génie, contacter Béatrice Laroche, Directrice Points Génie. Pour ce qui est en lien avec le fonctionnement du site web, contacter Raphaël Drouin, Webmestre. </blockquote>
       </div>
     );
   }
